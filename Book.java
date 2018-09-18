55eb79a79377fabc2d5e2b1d186647460912f6e9
daisryjoy <43154657+daisryjoy@users.noreply.github.com>
2018-09-17 20:29:47 -0400

    up to 2.86

--- Book.java
+++ Book.java
@@ -3,24 +3,73 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Daisry Joy Ladignon
+ * @version 2018.09.17
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages; 
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+   
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages; 
+    }
+    
+    /*
+     public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+    
+    public void setTitle()
+    {
+        this.title = title;
+    }
+    */
+    
+    
+    
+    public printAuthor()
+    {
+        
+    }
+    
+    public printTitle()
+    {
+        
+    }
+    
+    public printDetails()
+    {
+        
+    }
+    
+    
 }
